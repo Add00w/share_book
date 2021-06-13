5209db974901060f3c92a5f2a024f998253c2d4c
Abdullah A. Addow <abdullahiaddow@bootfi.com>
2021-06-13 11:21:09 +0300

    inititail commit

---
+++ lib/blocs/navigator/navigator_cubit.dart
@@ -0,0 +1,8 @@
+// import 'package:bloc/bloc.dart';
+// import 'package:meta/meta.dart';
+
+// part 'navigator_state.dart';
+
+// class NavigatorCubit extends Cubit<NavigatorState> {
+//   NavigatorCubit() : super(NavigatorInitial());
+// }
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:share_book/src/share_book_app.dart';
+
+void main() {
+  runApp(SharedBookApp());
+}
