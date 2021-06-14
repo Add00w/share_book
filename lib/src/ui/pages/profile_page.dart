e7309623af2161510be11ca4a483add6886b6429
Abdullahi Addow <jiincade@gmail.com>
2021-06-14 19:52:59 +0300

    add profile page

--- lib/src/ui/pages/profile_page.dart
+++ lib/src/ui/pages/profile_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class ShareBookApp extends StatelessWidget {
-  const ShareBookApp({Key? key}) : super(key: key);
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
