984ebd28b135cf4ba6b80b924f55d64835adbb6d
Abdullahi Addow <jiincade@gmail.com>
2021-06-14 19:54:31 +0300

    splash

--- lib/src/ui/pages/splash_page.dart
+++ lib/src/ui/pages/splash_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:share_book/src/ui/pages/home_page.dart';
+
+import 'main_page.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -11,21 +12,45 @@ class SplashPage extends StatefulWidget {
 class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
-    Future.delayed(Duration(seconds: 5), () {
+    Future.delayed(Duration(seconds: 2), () {
       Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => HomePage()));
+        context,
+        MaterialPageRoute(
+          builder: (context) => MainPage(),
+        ),
+      );
     });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color.fromRGBO(226, 89, 148, 1),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [Image.asset('assets/images/splash/splash.png')],
+    return ClipPath(
+      clipper: SplashClipper(),
+      child: Container(
+        color: Theme.of(context).primaryColor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Image.asset('assets/images/splash/splash.png')],
+        ),
       ),
     );
   }
+}
+
+class SplashClipper extends CustomClipper<Path> {
+  @override
+  getClip(Size size) {
+    final path = Path();
+    path.moveTo(size.width / 9, size.height);
+    path.addPolygon([
+      Offset.fromDirection(50),
+      Offset(size.width / 20, size.height / 5),
+      Offset(0, size.height)
+    ], true);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper oldClipper) => false;
 }
