1b886df4d63f3b949963cbd9fd88b6020f04c824
Abdullahi Addow <jiincade@gmail.com>
2022-07-21 11:49:41 +0300

    show runApp only from the material library.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:share_book/share_book_app.dart';
+import 'package:flutter/material.dart' show runApp;
+
+import 'share_book_app.dart';
 
 void main() {
   runApp(SharedBookApp());
