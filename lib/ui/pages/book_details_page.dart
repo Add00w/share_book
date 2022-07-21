c939499827a339531d3ca2f97242ead3792bbad9
Abdullahi Addow <jiincade@gmail.com>
2022-07-21 11:47:39 +0300

    fixed deprecated issues.

--- lib/ui/pages/book_details_page.dart
+++ lib/ui/pages/book_details_page.dart
@@ -17,7 +17,7 @@ class BookDetailsPage extends StatelessWidget {
                   width: double.infinity,
                   height: MediaQuery.of(context).size.height / 2.5,
                   decoration: BoxDecoration(
-                    color: Theme.of(context).accentColor,
+                    color: Theme.of(context).colorScheme.secondary,
                     image: DecorationImage(
                       image: AssetImage(book['image']!),
                     ),
@@ -65,7 +65,9 @@ class BookDetailsPage extends StatelessWidget {
                           style: Theme.of(context)
                               .textTheme
                               .headline6!
-                              .copyWith(color: Theme.of(context).accentColor)),
+                              .copyWith(
+                                  color:
+                                      Theme.of(context).colorScheme.secondary)),
                       Positioned(
                           top: 0.0,
                           left: 0.0,
@@ -74,7 +76,10 @@ class BookDetailsPage extends StatelessWidget {
                             style: Theme.of(context)
                                 .textTheme
                                 .headline6!
-                                .copyWith(color: Theme.of(context).accentColor),
+                                .copyWith(
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .secondary),
                           )),
                     ],
                   ),
@@ -114,9 +119,12 @@ class BookDetailsPage extends StatelessWidget {
                         children: [
                           Padding(
                             padding: const EdgeInsets.all(8.0),
-                            child: Text(
+                            child: SingleChildScrollView(
+                              child: Text(
                                 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'
-                                'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.'),
+                                'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+                              ),
+                            ),
                           ),
                           Padding(
                             padding: const EdgeInsets.all(8.0),
